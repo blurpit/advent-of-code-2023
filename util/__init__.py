4caede82bf51905376cf5b0504edc4490aa4b296
blurpit <davidschneider821@gmail.com>
2023-12-10 15:54:59 -0700

    Add Graph util

---
+++ util/__init__.py
@@ -0,0 +1,74 @@
+from collections import deque
+
+
+class Bag:
+    def push(self, item):
+        raise NotImplemented
+
+    def pop(self):
+        raise NotImplemented
+
+    def __len__(self):
+        raise NotImplemented
+
+    def __bool__(self):
+        return len(self) > 0
+
+class Stack(Bag):
+    def __init__(self):
+        self._data = []
+
+    def push(self, item):
+        self._data.append(item)
+
+    def pop(self):
+        return self._data.pop()
+
+    def __len__(self):
+        return len(self._data)
+
+    def __bool__(self):
+        return len(self) > 0
+
+class Queue(Bag):
+    def __init__(self):
+        self._data = deque()
+
+    def push(self, item):
+        self._data.append(item)
+
+    def pop(self):
+        return self._data.popleft()
+
+    def __len__(self):
+        return len(self._data)
+
+    def __bool__(self):
+        return len(self) > 0
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = set()
+        self.edges = dict()
+
+    def add_edge(self, v, w):
+        self.vertices.add(v)
+        self.vertices.add(w)
+        if v not in self.edges:
+            self.edges[v] = set()
+        self.edges[v].add(w)
+
+    def get_neighbors(self, v):
+        return self.edges.get(v, set())
+
+    def wfs(self, s, bag: Bag, on_marked):
+        marked = set()
+        bag.push(s)
+        while bag:
+            v = bag.pop()
+            if v not in marked:
+                marked.add(v)
+                on_marked(v)
+                for w in self.get_neighbors(v):
+                    bag.push(w)
